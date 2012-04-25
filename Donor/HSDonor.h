4eab2594e31c0dc8dbfcc823c4db85f79bc39ec3
Trifonov Alexander <fgengine@gmail.com>
2012-04-26 00:09:10 +0400

    Структура хранения настроек и разные полезные мелочи

--- Donor/HSDonor.h
+++ Donor/HSDonor.h
@@ -1,14 +1,83 @@
 /*--------------------------------------------------*/
 
-#import <Foundation/Foundation.h>
+#import "HSUserDefaults.h"
+
+/*--------------------------------------------------*/
+
+enum
+{
+    HSDonorSexUnknown,
+    HSDonorSexMan,
+    HSDonorSexWoman
+};
+
+typedef NSInteger HSDonorSex;
+
+/*--------------------------------------------------*/
+
+enum
+{
+    HSDonorBloodGroupUnknown,
+    HSDonorBloodGroupI,
+    HSDonorBloodGroupII,
+    HSDonorBloodGroupIII,
+    HSDonorBloodGroupIV
+};
+
+typedef NSInteger HSDonorBloodGroup;
+
+/*--------------------------------------------------*/
+
+enum
+{
+    HSDonorBloodRhUnknown,
+    HSDonorBloodRhPos,
+    HSDonorBloodRhNeg
+};
+
+typedef NSInteger HSDonorBloodRh;
 
 /*--------------------------------------------------*/
 
 @interface HSDonor : NSObject
 {
+    NSString* mLogin;
+    NSString* mPassword;
+    NSString* mName;
+    HSDonorSex mSex;
+    HSDonorBloodGroup mBloodGroup;
+    HSDonorBloodRh mBloodRh;
+    BOOL mVerifyPassword;
+    BOOL mShowPushNotice;
+    BOOL mSearchBloodGroup;
+    BOOL mCalendarReminders;
+    BOOL mCalendarCloseEvent;
+    BOOL mBloodUsePlatelets;
+    BOOL mBloodUseWhole;
+    BOOL mBloodUsePlasma;
 }
 
+@property (nonatomic, strong) NSString* login;
+@property (nonatomic, strong) NSString* password;
+@property (nonatomic, strong) NSString* name;
+@property (nonatomic, assign) HSDonorSex sex;
+@property (nonatomic, assign) HSDonorBloodGroup bloodGroup;
+@property (nonatomic, assign) HSDonorBloodRh bloodRh;
+@property (nonatomic, assign) BOOL verifyPassword;
+@property (nonatomic, assign) BOOL showPushNotice;
+@property (nonatomic, assign) BOOL searchBloodGroup;
+@property (nonatomic, assign) BOOL calendarReminders;
+@property (nonatomic, assign) BOOL calendarCloseEvent;
+@property (nonatomic, assign) BOOL bloodUsePlatelets;
+@property (nonatomic, assign) BOOL bloodUseWhole;
+@property (nonatomic, assign) BOOL bloodUsePlasma;
+
 + (HSDonor*) shared;
+
+- (void) loginWithInternet;
+- (void) logoutWithInternet;
+
+- (void) synchronize;
 
 @end
 
--- Donor/HSDonor.m
+++ Donor/HSDonor.m
@@ -10,6 +10,21 @@ static HSDonor *HSDonorShared = nil;
 
 @implementation HSDonor
 
+@synthesize login = mLogin;
+@synthesize password = mPassword;
+@synthesize name = mName;
+@synthesize sex = mSex;
+@synthesize bloodGroup = mBloodGroup;
+@synthesize bloodRh = mBloodRh;
+@synthesize verifyPassword = mVerifyPassword;
+@synthesize showPushNotice = mShowPushNotice;
+@synthesize searchBloodGroup = mSearchBloodGroup;
+@synthesize calendarReminders = mCalendarReminders;
+@synthesize calendarCloseEvent = mCalendarCloseEvent;
+@synthesize bloodUsePlatelets = mBloodUsePlatelets;
+@synthesize bloodUseWhole = mBloodUseWhole;
+@synthesize bloodUsePlasma = mBloodUsePlasma;
+
 + (HSDonor*) shared
 {
     @synchronized(self)
@@ -22,9 +37,36 @@ static HSDonor *HSDonorShared = nil;
     return HSDonorShared;
 }
 
-+ (id) allocWithZone:(NSZone *)zone
++ (id) allocWithZone:(NSZone*)zone
 {
     return [[self shared] retain];
+}
+
+- (id) init
+{
+    self = [super init];
+    if(self != nil)
+    {
+        NSUserDefaults* user = [NSUserDefaults standardUserDefaults];
+        if(user != nil)
+        {
+            mLogin = [user stringForKey:@"Login" asDefaults:@""];
+            mPassword = [user stringForKey:@"Password" asDefaults:@""];
+            mName = [user stringForKey:@"Name" asDefaults:@""];
+            mSex = [user integerForKey:@"Sex" asDefaults:HSDonorSexUnknown];
+            mBloodGroup = [user integerForKey:@"BloodGroup" asDefaults:HSDonorBloodGroupUnknown];
+            mBloodRh = [user integerForKey:@"BloodRh" asDefaults:HSDonorBloodRhUnknown];
+            mVerifyPassword = [user boolForKey:@"VerifyPassword" asDefaults:NO];
+            mShowPushNotice = [user boolForKey:@"ShowPushNotice" asDefaults:NO];
+            mSearchBloodGroup = [user boolForKey:@"SearchBloodGroup" asDefaults:NO];
+            mCalendarReminders = [user boolForKey:@"CalendarReminders" asDefaults:NO];
+            mCalendarCloseEvent = [user boolForKey:@"CalendarCloseEvent" asDefaults:NO];
+            mBloodUsePlatelets = [user boolForKey:@"BloodUsePlatelets" asDefaults:NO];
+            mBloodUseWhole = [user boolForKey:@"BloodUseWhole" asDefaults:NO];
+            mBloodUsePlasma = [user boolForKey:@"BloodUsePlasma" asDefaults:NO];
+        }
+    }
+    return self;
 }
 
 - (id) copyWithZone:(NSZone*)zone
@@ -45,6 +87,36 @@ static HSDonor *HSDonorShared = nil;
 - (id) autorelease
 {
     return self;
+}
+
+- (void) loginWithInternet
+{
+}
+
+- (void) logoutWithInternet
+{
+}
+
+- (void) synchronize
+{
+    NSUserDefaults* user = [NSUserDefaults standardUserDefaults];
+    if(user != nil)
+    {
+        [user setObject:mLogin forKey:@"Login"];
+        [user setObject:mPassword forKey:@"Password"];
+        [user setObject:mName forKey:@"Name"];
+        [user setInteger:mSex forKey:@"Sex"];
+        [user setInteger:mBloodGroup forKey:@"BloodGroup"];
+        [user setInteger:mBloodRh forKey:@"BloodRh"];
+        [user setBool:mVerifyPassword forKey:@"VerifyPassword"];
+        [user setBool:mShowPushNotice forKey:@"ShowPushNotice"];
+        [user setBool:mSearchBloodGroup forKey:@"SearchBloodGroup"];
+        [user setBool:mCalendarReminders forKey:@"CalendarReminders"];
+        [user setBool:mCalendarCloseEvent forKey:@"CalendarCloseEvent"];
+        [user setBool:mBloodUsePlatelets forKey:@"BloodUsePlatelets"];
+        [user setBool:mBloodUseWhole forKey:@"BloodUseWhole"];
+        [user setBool:mBloodUsePlasma forKey:@"BloodUsePlasma"];
+    }
 }
 
 @end
