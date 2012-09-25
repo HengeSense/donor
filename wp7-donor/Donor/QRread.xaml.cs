3581fa01c0d6114494c56cccb03af12b83061284
Anton Lenev <m0rg0t.Anton@gmail.com>
2012-09-25 19:53:56 +0400

    QR code init commit

---
+++ wp7-donor/Donor/QRread.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using com.google.zxing.qrcode;
+using Microsoft.Devices;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
+using System.Windows.Navigation;
+using com.google.zxing;
+using com.google.zxing.common;
+
+namespace Donor
+{
+    public class PhotoCameraLuminanceSource : LuminanceSource
+    {
+        public byte[] PreviewBufferY { get; private set; }
+
+        public PhotoCameraLuminanceSource(int width, int height)
+            : base(width, height)
+        {
+            PreviewBufferY = new byte[width * height];
+        }
+
+        public override sbyte[] Matrix
+        {
+            get { return (sbyte[])(Array)PreviewBufferY; }
+        }
+
+        public override sbyte[] getRow(int y, sbyte[] row)
+        {
+            if (row == null || row.Length < Width)
+            {
+                row = new sbyte[Width];
+            }
+
+            for (int i = 0; i < Height; i++)
+                row[i] = (sbyte)PreviewBufferY[i * Width + y];
+
+            return row;
+        }
+    }
+
+    public partial class QRread : PhoneApplicationPage
+    {
+        private readonly DispatcherTimer _timer;
+        private readonly ObservableCollection<string> _matches;
+
+        private PhotoCameraLuminanceSource _luminance;
+        private QRCodeReader _reader;
+        private PhotoCamera _photoCamera;
+
+        public QRread()
+        {
+            InitializeComponent();
+
+            _matches = new ObservableCollection<string>();
+            _matchesList.ItemsSource = _matches;
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(250);
+            _timer.Tick += (o, arg) => ScanPreviewBuffer();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            _photoCamera = new PhotoCamera();
+            _photoCamera.Initialized += OnPhotoCameraInitialized;
+            _previewVideo.SetSource(_photoCamera);
+
+            CameraButtons.ShutterKeyHalfPressed += (o, arg) => _photoCamera.Focus();
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void OnPhotoCameraInitialized(object sender, CameraOperationCompletedEventArgs e)
+        {
+            int width = Convert.ToInt32(_photoCamera.PreviewResolution.Width);
+            int height = Convert.ToInt32(_photoCamera.PreviewResolution.Height);
+
+            _luminance = new PhotoCameraLuminanceSource(width, height);
+            _reader = new QRCodeReader();
+
+            Dispatcher.BeginInvoke(() =>
+            {
+                _previewTransform.Rotation = _photoCamera.Orientation;
+                _timer.Start();
+            });
+        }
+
+        private void ScanPreviewBuffer()
+        {
+            try
+            {
+                _photoCamera.GetPreviewBufferY(_luminance.PreviewBufferY);
+                var binarizer = new HybridBinarizer(_luminance);
+                var binBitmap = new BinaryBitmap(binarizer);
+                var result = _reader.decode(binBitmap);
+                Dispatcher.BeginInvoke(() => DisplayResult(result.Text));
+            }
+            catch
+            {
+            }
+        }
+
+        private void DisplayResult(string text)
+        {
+            if (!_matches.Contains(text))
+                _matches.Add(text);
+        }
+        
+
+
+    }
+}
