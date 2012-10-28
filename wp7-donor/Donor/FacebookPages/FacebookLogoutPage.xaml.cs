8dbfed68000c389e1987587aadfa3f925025cdf7
Anton Lenev <m0rg0t.Anton@gmail.com>
2012-10-28 15:14:25 +0400

    QR code read page

---
+++ wp7-donor/Donor/FacebookPages/FacebookLogoutPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Facebook;
+using Donor;
+
+namespace facebook_windows_phone_sample.Pages
+{
+    public partial class FacebookLogoutPage : PhoneApplicationPage
+    {
+        private const string AppId = "438918122827407";
+
+        /// <summary>
+        /// Extended permissions is a comma separated list of permissions to ask the user.
+        /// </summary>
+        /// <remarks>
+        /// For extensive list of available extended permissions refer to 
+        /// https://developers.facebook.com/docs/reference/api/permissions/
+        /// </remarks>
+        private const string ExtendedPermissions = "user_about_me,read_stream,publish_stream";
+
+        private readonly FacebookClient _fb = new FacebookClient();
+
+        public FacebookLogoutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_Loaded(object sender, RoutedEventArgs e)
+        {
+            //var logoutUrl = GetFacebookLogoutUrl(AppId, ExtendedPermissions);
+            //webBrowser1.Navigate(logoutUrl);
+
+            string logout_format = "http://www.facebook.com/logout.php?api_key={0}&session_key={1}&next={2}";
+            string access_token = HttpUtility.UrlDecode(App.ViewModel.User.FacebookToken);
+            //char[] tokenSeparator = new char[] { '|' };
+            //string session = access_token.Split(tokenSeparator)[1];
+            string session = App.ViewModel.User.FacebookToken;
+
+            var parameters = new Dictionary<string, object>();
+            webBrowser1.Navigate(_fb.GetLogoutUrl(parameters));
+        }
+
+        private Uri GetFacebookLogoutUrl(string appId, string extendedPermissions)
+        {
+            var parameters = new Dictionary<string, object>();
+            //parameters["client_id"] = appId;
+            //parameters["redirect_uri"] = "https://www.facebook.com/logout.php";
+            parameters["next"] = "https://www.facebook.com/logout.php";
+            //parameters["display"] = "touch";
+            parameters["access_token"] = App.ViewModel.User.FacebookToken;
+
+            return _fb.GetLogoutUrl(parameters);
+        }
+
+        private void webBrowser1_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            this.NavigationService.GoBack();
+            /*FacebookOAuthResult oauthResult;
+            if (!_fb.TryParseOAuthCallbackUrl(e.Uri, out oauthResult))
+            {
+                return;
+            }
+
+            if (oauthResult.IsSuccess)
+            {
+                var accessToken = oauthResult.AccessToken;
+                LoginSucceded(accessToken);
+            }
+            else
+            {
+                // user cancelled
+                MessageBox.Show(oauthResult.ErrorDescription);
+            }*/
+        }
+    }
+}
---
+++ wp7-donor/Donor/QRread.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using com.google.zxing.qrcode;
+using Microsoft.Devices;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
+using System.Windows.Navigation;
+using com.google.zxing;
+using com.google.zxing.common;
+using Donor.ViewModels;
+
+namespace Donor
+{
+
+
+    public partial class QRread : PhoneApplicationPage
+    {
+        private readonly DispatcherTimer _timer;
+        private readonly ObservableCollection<string> _matches;
+
+        private PhotoCameraLuminanceSource _luminance;
+        private QRCodeReader _reader;
+        private PhotoCamera _photoCamera;
+
+        public QRread()
+        {
+            InitializeComponent();
+
+            _matches = new ObservableCollection<string>();
+            _matchesList.ItemsSource = _matches;
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(250);
+            _timer.Tick += (o, arg) => ScanPreviewBuffer();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            try
+            {
+                //if (App.ViewModel.Qr.CameraFocusSet == false)
+                //{
+                    _photoCamera = new PhotoCamera();
+                    _photoCamera.Initialized += OnPhotoCameraInitialized;
+                    _previewVideo.SetSource(_photoCamera);
+                //}
+            }
+            catch { };
+
+            try
+            {
+                if (App.ViewModel.Qr.CameraFocusSet == false)
+                {
+                    CameraButtons.ShutterKeyHalfPressed += (o, arg) => { try { _photoCamera.Focus(); } catch { }; };
+                    App.ViewModel.Qr.CameraFocusSet = true;
+                };
+            }
+            catch
+            {
+            };
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void OnPhotoCameraInitialized(object sender, CameraOperationCompletedEventArgs e)
+        {
+            int width = Convert.ToInt32(_photoCamera.PreviewResolution.Width);
+            int height = Convert.ToInt32(_photoCamera.PreviewResolution.Height);
+
+            _luminance = new PhotoCameraLuminanceSource(width, height);
+            _reader = new QRCodeReader();
+
+            Dispatcher.BeginInvoke(() =>
+            {
+                _previewTransform.Rotation = _photoCamera.Orientation;
+                _timer.Start();
+            });
+        }
+
+        private void ScanPreviewBuffer()
+        {
+            try
+            {
+                _photoCamera.GetPreviewBufferY(_luminance.PreviewBufferY);
+                var binarizer = new HybridBinarizer(_luminance);
+                var binBitmap = new BinaryBitmap(binarizer);
+                var result = _reader.decode(binBitmap);
+                Dispatcher.BeginInvoke(() => DisplayResult(result.Text));
+            }
+            catch
+            {
+            }
+        }
+
+        private void DisplayResult(string text)
+        {
+            _timer.Stop();
+            //_photoCamera.Dispose();
+            try
+            {
+                MessageBox.Show("Добавлен QR код: " + text);
+                App.ViewModel.Qr.QRcode = text;
+                try
+                {
+                    this.NavigationService.GoBack();
+                }
+                catch { };
+            }
+            catch { };
+        }
+
+
+    }
+}
