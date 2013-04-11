1bb156e8132da403917a50539dde44987e8bf4d9
Anton Lenev <m0rg0t.Anton@gmail.com>
2013-04-11 15:17:45 +0400

    DWP-351

--- wp7-donor/Donor/ViewModels/BadgesViewModel.cs
+++ wp7-donor/Donor/ViewModels/BadgesViewModel.cs
@@ -423,7 +423,7 @@ namespace Donor.ViewModels
                     request.AddParameter("user_token", user_token);
                     request.AddParameter("badge_name", "donorfriend");
                     //for test
-                    request.AddParameter("unique", "f");
+                    //request.AddParameter("unique", "f");
 
                     client.ExecuteAsync(request, response =>
                     {
